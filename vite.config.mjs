7f5adbfa9aaa6c44c8fdedd91ad9013e8a940fad
Muzamal Ali <muzamalali7755@gmail.com>
2025-11-25 10:30:57 +0500

    server host fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -75,6 +75,9 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    allowedHosts: [
+      'tryfann.globaltechserivce.com',
+    ],
   },
   preview: {
     host: true,
